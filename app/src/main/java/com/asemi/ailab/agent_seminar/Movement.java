9866a6d95d284847fbfafe944413dd1294e6a325
Wataru.B <acha2689@chiba-u.jp>
2017-02-17 15:41:40 +0900

    諜略カード等の効果に関する関数の微修正（実装途中につきエラー多発）

--- app/src/main/java/com/asemi/ailab/agent_seminar/Movement.java
+++ app/src/main/java/com/asemi/ailab/agent_seminar/Movement.java
@@ -1,6 +1,7 @@
 package com.asemi.ailab.agent_seminar;
 
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -72,23 +73,26 @@ public interface MovementFunc {
     // 山札からドロー
     public ArrayList<StrategyCard> Draw(Player player, int num);
     // 諜略カードを保有
-    public void Possession(StrategyCard strategyCard, Where where);
+    public void Possession(ArrayList<StrategyCard> strategyCards);
     // 手札からカードを捨てる
-    public void Dump(StrategyCard strategyCard, int num);
+    public void Dump(ArrayList<StrategyCard> strategyCards);
     // プレイヤーをロックオン状態にする
     public void LockOn(Player player);
     // プレイヤーをロスト状態にする
     public void Lost(Player player);
     // 裏側状態の送信メッセージを解読する
-    public void Decode(StrategyCard strategyCard);
+    public void Decode(Player player, ArrayList<StrategyCard> strategyCard);
     // 山札をシャッフルする
     public void Shuffle();
     // 手札の諜略カードを山札に戻す
-    public void PutOnTheDeck(StrategyCard strategyCard);
+    public void PutOnTheDeck(ArrayList<StrategyCard> strategyCards);
     // 保有しているカードを削除する
-    public void Delete(StrategyCard strategyCard);
+    public void Delete(ArrayList<StrategyCard> strategyCard);
     // 特定のプレイヤーからカードを奪う
-    public ArrayList<StrategyCard> Steal(Player player, int num);
+    public ArrayList<StrategyCard> Steal(Player player, Player enemy, ArrayList<StrategyCard> strategyCards;
     // 自分もしくは相手の手札カードを選ぶ
     public ArrayList<StrategyCard> Choose(Where where, int num);
+    // 保有したカードを奪う
+    public ArrayList<StrategyCard> PickUp();
+
 }
