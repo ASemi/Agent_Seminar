132daec6c1cbdbaf1a73f5c92703a9bfe5de0bff
shioya <almond776@gmail.com>
2017-02-18 20:26:08 +0900

    異界探偵を押すと喋る機能の追加 フルスクリーン化と背景色を黒に変更

--- app/src/main/java/com/asemi/ailab/agent_seminar/Title.java
+++ app/src/main/java/com/asemi/ailab/agent_seminar/Title.java
@@ -1,17 +1,24 @@
 package com.asemi.ailab.agent_seminar;
 
+import android.app.Activity;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class Title extends AppCompatActivity implements View.OnClickListener {
+public class Title extends Activity implements View.OnClickListener {
 
     private Button button1;
     private TextView txt1, txt2;
+
+    private ImageButton imagebutton1;
+
     final int MAX_PLAYER = 1;
     Movement movement = new Movement();
     AllDeck allDeck = new AllDeck();
@@ -30,6 +37,9 @@ public class Title extends AppCompatActivity implements View.OnClickListener {
                 allDeck.alldeck.removeFirst();
                 //textView.setText("button!");
                 break;
+            case R.id.imageButton1:
+                txt1.setText("証拠なし……か");
+                break;
         }
     }
 
@@ -37,10 +47,32 @@ public class Title extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decor = this.getWindow().getDecorView();
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
         setContentView(R.layout.activity_title);
         txt1 = (TextView) findViewById(R.id.txt1);
         txt2 = (TextView) findViewById(R.id.txt2);
         button1 = (Button) findViewById(R.id.button);
         button1.setOnClickListener(this);
+
+        imagebutton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imagebutton1.setOnClickListener(this);
+
+        txt1 = (TextView) findViewById(R.id.txt1);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decor = this.getWindow().getDecorView();
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
